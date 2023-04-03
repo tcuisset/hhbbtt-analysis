06e61828018b03319198251cccf4dadb035e5b68
Jaime Leon <jleonhol@gmail.com>
2023-04-03 15:57:00 +0200

    New module for filtering ditau+jet events

--- config/llr_2018.py
+++ config/llr_2018.py
@@ -262,15 +262,25 @@ class Config(cmt_config):
 
             # weights
             Feature("PUjetID_SF", "PUjetID_SF", binning=(100, 0, 2),
-                x_title=Label("PUjetID_SF"), tags=["weight"]),
-            Feature("PUReweight", "PUReweight", binning=(100, 0, 2),
+                x_title=Label("PUjetID_SF"), tags=["weight"], systematic=["pujetsyst"]),
+            Feature("PUReweight", "PUReweight", binning=(100, 0, 0.05),
                 x_title=Label("PUReweight"), tags=["weight"]),
             Feature("bTagweightReshape", "bTagweightReshape", binning=(100, 0, 2),
-                x_title=Label("bTagweightReshape"), tags=["weight"]),
+                x_title=Label("bTagweightReshape"), tags=["weight"],
+                systematics=["jet_1", "jet_2", "jet_3", "jet_4", "jet_5", "jet_6",
+                    "jet_7", "jet_8", "jet_9", "jet_10", "jet_11",
+                    "jes", "ls", "hf", "hfstats1", "hfstats2", "lfstats1", "cferr1", "cferr2"]),
             Feature("trigSF", "trigSF", binning=(100, 0, 2),
-                x_title=Label("trigSF"), tags=["weight"]),
-            Feature("IdAndIsoSF_deep_pt", "IdAndIsoSF_deep_pt", binning=(100, 0, 2),
-                x_title=Label("IdAndIsoSF_deep_pt"), tags=["weight"]),
+                x_title=Label("trigSF"), tags=["weight"],
+                systematics=["muon_general", "electron_general", "dm0", "dm1", "dm10", "dm11", "vbfjet"]),
+            Feature("IdAndIsoSF_deep_pt", "IdAndIsoAndFakeSF_deep_pt", binning=(100, 0, 2),
+                x_title=Label("IdAndIsoSF_deep_pt"), tags=["weight"],
+                systematics=["idAndIsoAndFakeSF_tauid_pt20to25", "idAndIsoAndFakeSF_tauid_pt25to30",
+                    "idAndIsoAndFakeSF_tauid_pt30to35", "idAndIsoAndFakeSF_tauid_pt35to40",
+                    "idAndIsoAndFakeSF_tauid_pt40toInf", "idAndIsoAndFakeSF_mutauFR_etaLt0p4",
+                    "idAndIsoAndFakeSF_mutauFR_eta0p4to0p8", "idAndIsoAndFakeSF_mutauFR_eta0p8to1p2",
+                    "idAndIsoAndFakeSF_mutauFR_eta1p2to1p7", "idAndIsoAndFakeSF_mutauFR_etaGt1p7",
+                    "idAndIsoAndFakeSF_etauFR_barrel", "idAndIsoAndFakeSF_etauFR_endcap"]),
             Feature("smearing_factor", "{bjet1_smearFactor, bjet1_smearFactor2}", binning=(100, 0, 2),
                 x_title=Label("smearing_factor"), tags=["weight"]),
             Feature("L1pref_weight", "L1pref_weight", binning=(100, 0, 2),
@@ -278,6 +288,13 @@ class Config(cmt_config):
             Feature("prescaleWeight", "prescaleWeight", binning=(100, 0, 2),
                 x_title=Label("prescaleWeight"), tags=["weight"]),
 
+            # DNN
+            Feature("dnn", "DNNoutSM_kl_1", binning=(50, 0, 1),
+                x_title=Label("DNN output"),
+                systematics=["muon", "electron_dm0", "electron_dm1",
+                    "tau_dm0", "tau_dm1", "tau_dm10", "tau_dm11",
+                    "jet_1", "jet_2", "jet_3", "jet_4", "jet_5", "jet_6",
+                    "jet_7", "jet_8", "jet_9", "jet_10", "jet_11"]),
         ]
         return ObjectCollection(features)
         
@@ -331,6 +348,12 @@ class Config(cmt_config):
                 folder=os.path.join(skim_directory, "SKIM_GGHH_NLO_cHHH1_xs"),
                 process=self.processes.get("ggf_sm"),
                 file_pattern="output_.*root",
+                xs=1.),  # already normalised to xs
+
+            Dataset("ggf_sm_syst",
+                folder=os.path.join(skim_directory, "SKIM_GGHH_NLO_cHHH1_xs"),
+                process=self.processes.get("ggf_sm"),
+                file_pattern="syst_output_.*root",
                 xs=1.),  # already normalised to xs
 
             Dataset("ggf_0_1",
@@ -410,6 +433,57 @@ class Config(cmt_config):
         # weights.channels_mult = {channel: jrs(weights.channels[channel], op="*")
             # for channel in weights.channels}
         return weights
+
+    def add_systematics(self):
+        systematics = [
+            Systematic("muon_general", "_mu"),
+            Systematic("electron_general", "_ele"),
+            Systematic("dm0", "_DM0"),
+            Systematic("dm1", "_DM1"),
+            Systematic("dm10", "_DM10"),
+            Systematic("dm11", "_DM11"),
+            Systematic("muon", "_mu", up="up", down="down"),
+            Systematic("electron_dm0", "_ele", up="up_DM0", down="down_DM0"),
+            Systematic("electron_dm1", "_ele", up="up_DM1", down="down_DM1"),
+            Systematic("tau_dm0", "_tau", up="up_DM0", down="down_DM0"),
+            Systematic("tau_dm1", "_tau", up="up_DM1", down="down_DM1"),
+            Systematic("tau_dm10", "_tau", up="up_DM10", down="down_DM10"),
+            Systematic("tau_dm11", "_tau", up="up_DM11", down="down_DM11"),
+            Systematic("jet_1", "_jet", up="up1", down="down1"),
+            Systematic("jet_2", "_jet", up="up2", down="down2"),
+            Systematic("jet_3", "_jet", up="up3", down="down3"),
+            Systematic("jet_4", "_jet", up="up4", down="down4"),
+            Systematic("jet_5", "_jet", up="up5", down="down5"),
+            Systematic("jet_6", "_jet", up="up6", down="down6"),
+            Systematic("jet_7", "_jet", up="up7", down="down7"),
+            Systematic("jet_8", "_jet", up="up8", down="down8"),
+            Systematic("jet_9", "_jet", up="up9", down="down9"),
+            Systematic("jet_10", "_jet", up="up10", down="down10"),
+            Systematic("jet_11", "_jet", up="up11", down="down11"),
+            Systematic("pujetsyst", ""),
+            Systematic("jes", "_jes"),
+            Systematic("ls", "_lf"),
+            Systematic("hf", "_hf"),
+            Systematic("hfstats1", "_hfstats1"),
+            Systematic("hfstats2", "_hfstats2"),
+            Systematic("lfstats1", "_lfstats1"),
+            Systematic("cferr1", "_cferr1"),
+            Systematic("cferr2", "_cferr2"),
+            Systematic("vbfjet", "_vbfjet"),
+            Systematic("idAndIsoAndFakeSF_tauid_pt20to25", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_tauid_pt20to25")),
+            Systematic("idAndIsoAndFakeSF_tauid_pt25to30", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_tauid_pt25to30")),
+            Systematic("idAndIsoAndFakeSF_tauid_pt30to35", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_tauid_pt30to35")),
+            Systematic("idAndIsoAndFakeSF_tauid_pt35to40", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_tauid_pt35to40")),
+            Systematic("idAndIsoAndFakeSF_tauid_pt40toInf", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_tauid_pt40toInf")),
+            Systematic("idAndIsoAndFakeSF_mutauFR_etaLt0p4", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_mutauFR_etaLt0p4")),
+            Systematic("idAndIsoAndFakeSF_mutauFR_eta0p4to0p8", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_mutauFR_eta0p4to0p8")),
+            Systematic("idAndIsoAndFakeSF_mutauFR_eta0p8to1p2", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_mutauFR_eta0p8to1p2")),
+            Systematic("idAndIsoAndFakeSF_mutauFR_eta1p2to1p7", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_mutauFR_eta1p2to1p7")),
+            Systematic("idAndIsoAndFakeSF_mutauFR_etaGt1p7", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_mutauFR_etaGt1p7")),
+            Systematic("idAndIsoAndFakeSF_etauFR_barrel", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_etauFR_barrel")),
+            Systematic("idAndIsoAndFakeSF_etauFR_endcap", ("IdAndIsoAndFakeSF_deep_pt", "idAndIsoAndFakeSF_etauFR_endcap")),
+        ]
+        return ObjectCollection(systematics)
 
 
 config = Config("base", year=2018, ecm=13, lumi_pb=59741)
