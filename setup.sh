c94036ce66a0a1d5b8fb96f4092cf23390aea9fd
Jaime Leon <jleonhol@gmail.com>
2023-05-29 12:07:28 +0200

    Modified sphinx version to remove compilation errors

--- setup.sh
+++ setup.sh
@@ -310,7 +310,7 @@ action() {
             cmt_pip_install --no-deps git+https://github.com/riga/law
             cmt_pip_install --no-deps git+https://github.com/riga/plotlib
             cmt_pip_install --no-deps gast==0.2.2  # https://github.com/tensorflow/autograph/issues/1
-            cmt_pip_install sphinx
+            cmt_pip_install sphinx==5.2.2
             cmt_pip_install sphinx_rtd_theme
             cmt_pip_install sphinx_design
         fi
