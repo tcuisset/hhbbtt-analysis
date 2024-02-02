917d132c8f6d702f7036d9b6e68f52a6c169c916
Elena Vernazza <e.vernazza1@campus.unimib.it>
2024-02-02 15:27:20 +0100

    Update instructions

--- ToRun/law_commands_2018_ZZ_0.sh
+++ ToRun/law_commands_2018_ZZ_0.sh
@@ -4,12 +4,25 @@
 ###############################################################################################################################################
 
 ###############################################################################################################################################
-# COMMON STEP TO ALL THE ANALYES
+# COMMON STEP TO ALL THE ANALYES: DONE
 ###############################################################################################################################################
 
-# before launching, remove changes in boosted category
 law run PreprocessRDFWrapper --version prod_240128 --category-names base_selection --config-name ul_2018_ZZ_v10 \
- --dataset-names zz_sl_signal,zz_sl_background,zh_htt,zh_hbb_zll,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,zh_hbb_zqq,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm \
+ --dataset-names zz_sl_signal,zz_sl_background,zh_htt,zh_hbb_zll,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm \
+ --PreprocessRDF-modules-file modulesrdf \
+ --PreprocessRDF-keep-and-drop-file keep_and_drop_file --workers 50 \
+ --PreprocessRDF-workflow htcondor --PreprocessRDF-htcondor-scheduler llrt3condor.in2p3.fr --PreprocessRDF-transfer-logs \
+ --PreprocessRDF-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=short,WNTag=el7'
+
+law run PreprocessRDFWrapper --version prod_240128 --category-names base_selection --config-name ul_2018_ZZ_v10 \
+ --dataset-names ggXZZbbtt_M200,ggXZZbbtt_M300,ggXZZbbtt_M400,ggXZZbbtt_M500,ggXZZbbtt_M600,ggXZZbbtt_M700,ggXZZbbtt_M800,ggXZZbbtt_M900,ggXZZbbtt_M1000,ggXZZbbtt_M1100,ggXZZbbtt_M1200,ggXZZbbtt_M1300,ggXZZbbtt_M1400,ggXZZbbtt_M1500,ggXZZbbtt_M2000,ggXZZbbtt_M3000 \
+ --PreprocessRDF-modules-file modulesrdf \
+ --PreprocessRDF-keep-and-drop-file keep_and_drop_file --workers 50 \
+ --PreprocessRDF-workflow htcondor --PreprocessRDF-htcondor-scheduler llrt3condor.in2p3.fr --PreprocessRDF-transfer-logs \
+ --PreprocessRDF-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=short,WNTag=el7'
+
+law run PreprocessRDFWrapper --version prod_240128 --category-names base_selection --config-name ul_2018_ZZ_v10 \
+ --dataset-names data_tau_a,data_tau_b,data_tau_c,data_tau_d,data_mutau_a,data_mutau_b,data_mutau_c,data_mutau_d,data_etau_a,data_etau_b,data_etau_c,data_etau_d \
  --PreprocessRDF-modules-file modulesrdf \
  --PreprocessRDF-keep-and-drop-file keep_and_drop_file --workers 50 \
  --PreprocessRDF-workflow htcondor --PreprocessRDF-htcondor-scheduler llrt3condor.in2p3.fr --PreprocessRDF-transfer-logs \
@@ -20,38 +33,58 @@ law run PreprocessRDFWrapper --version prod_240128 --category-names base_selecti
 ###############################################################################################################################################
 
 ###############################################################################################################################################
-# ELLIPTICAL MASS CUT
+# ELLIPTICAL MASS CUT: 45 missing (RESERV)
 ###############################################################################################################################################
 
 law run CategorizationWrapper --version prod_240128 --category-names base --config-name ul_2018_ZZ_v10 \
- --dataset-names zz_sl_signal,zz_sl_background \
+ --skip-dataset-tags secondary \
  --PreprocessRDF-version prod_240128 \
  --Categorization-base-category-name base_selection --Categorization-feature-modules-file modulesrdf_Ellipse --workers 50 \
  --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
  --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=reserv,WNTag=el7'
-# --skip-dataset-tags secondary \
 
 ###############################################################################################################################################
-# DNN INPUT
+# DNN INPUT: 40 missing (LONG)
 ###############################################################################################################################################
 
 law run CategorizationWrapper --version prod_240128 --category-names ZZ_elliptical_cut_80_sr --config-name ul_2018_ZZ_v10 \
- --dataset-names zz_sl_signal,zz_sl_background \
+ --skip-dataset-tags secondary \
  --Categorization-base-category-name base_selection \
  --Categorization-feature-modules-file modulesrdf_DNN  --Categorization-keep-and-drop-file keep_and_drop_file_DNN --workers 50 \
  --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
- --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=reserv,WNTag=el7'
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=long,WNTag=el7'
 
 ###############################################################################################################################################
 # ANALYSIS ETAU, MUTAU, TAUTAU
 ###############################################################################################################################################
 
-law run CategorizationWrapper --version prod_240128 --category-names ZZ_elliptical_cut_80_etau,ZZ_elliptical_cut_80_mutau,ZZ_elliptical_cut_80_tautau --config-name ul_2018_ZZ_v10 \
- --dataset-names zz_sl_signal,zz_sl_background \
+law run CategorizationWrapper --version prod_240128 --category-names ZZ_elliptical_cut_80_etau --config-name ul_2018_ZZ_v10 \
+ --skip-dataset-tags secondary \
+ --Categorization-base-category-name base_selection \
+ --Categorization-feature-modules-file modulesrdf_syst --workers 50 \
+ --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=short,WNTag=el7'
+
+law run CategorizationWrapper --version prod_240128 --category-names ZZ_elliptical_cut_80_etau,ZZ_elliptical_cut_80_mutau --config-name ul_2018_ZZ_v10 \
+ --dataset-names zz_sl_signal,zz_sl_background,zh_htt,zh_hbb_zll,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm \
  --Categorization-base-category-name base_selection \
  --Categorization-feature-modules-file modulesrdf_syst --workers 50 \
  --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
  --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=reserv,WNTag=el7'
+
+law run CategorizationWrapper --version prod_240128 --category-names ZZ_elliptical_cut_80_tautau --config-name ul_2018_ZZ_v10 \
+ --dataset-names zz_sl_signal,zz_sl_background,zh_htt,zh_hbb_zll,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm \
+ --Categorization-base-category-name base_selection \
+ --Categorization-feature-modules-file modulesrdf_syst --workers 50 \
+ --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=long,WNTag=el7'
+
+law run CategorizationWrapper --version prod_240128 --category-names ZZ_elliptical_cut_80_etau,ZZ_elliptical_cut_80_mutau,ZZ_elliptical_cut_80_tautau --config-name ul_2018_ZZ_v10 \
+ --dataset-names data_tau_a,data_tau_b,data_tau_c,data_tau_d,data_mutau_a,data_mutau_b,data_mutau_c,data_mutau_d,data_etau_a,data_etau_b,data_etau_c,data_etau_d \
+ --Categorization-base-category-name base_selection \
+ --Categorization-feature-modules-file modulesrdf_syst --workers 50 \
+ --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=long,WNTag=el7'
 
 ###############################################################################################################################################
 ############################################################  PRECOUNTER  #####################################################################
