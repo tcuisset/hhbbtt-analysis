7497e2e1f3426574349acce84fc6817e57dcafc0
Théo Cuisset <theo.cuisset@polytechnique.edu>
2024-02-07 18:01:22 +0100

    Updated ZH commands

--- ToRun/law_commands_2018_ZH_0.sh
+++ ToRun/law_commands_2018_ZH_0.sh
@@ -28,6 +28,8 @@ law run PreprocessRDFWrapper --version prod_240128 --category-names base_selecti
 # ul_2018_ZttHbb_v10 specific datasets
 # already run for ZZ : zz_sl_signal,zz_sl_background,zh_htt,zh_hbb_zll,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,zh_hbb_zqq,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm
 # already run for ul_2018_ZbbHtt_v10 : zz_sl
+ln -s /grid_mnt/data__data.polcms/cms/cuisset/cmt/PreprocessRDF/ul_2018_ZbbHtt_v10/zz_sl /grid_mnt/data__data.polcms/cms/cuisset/cmt/PreprocessRDF/ul_2018_ZttHbb_v10/zz_sl
+
 law run PreprocessRDFWrapper --version prod_240128 --category-names base_selection --config-name ul_2018_ZttHbb_v10 \
  --dataset-names zh_ztt_hbb_signal,zh_ztt_hbb_background,zh_htt \
  --PreprocessRDF-modules-file modulesrdf \
@@ -42,6 +44,8 @@ law run PreprocessRDFWrapper --version prod_240128 --category-names base_selecti
 # list of datasets running categroization for >2hours
 # tth_bb, st_tw_top, ttw_qq, ttwh, st_tw_antitop, wz_llqq, zh_htt, wplush_htt, wz_lnuqq, ewk_wminus, ww_lnuqq, ttww, wz_lllnu, tth_tautau, st_antitop, ttw_lnu, wminush_htt, ewk_wminus, wz_llqq, tth_bb
 
+# for dataset in tth_bb st_tw_top ttw_qq ttwh st_tw_antitop wz_llqq zh_htt wplush_htt wz_lnuqq ewk_wminus ewk_wplus ww_lnuqq ttww wz_lllnu tth_tautau st_antitop st_top ttw_lnu wminush_htt ewk_wminus wz_llqq tth_bb; do rm -r $dataset; done
+
 ###############################################################################################################################################
 # ELLIPTICAL MASS CUT
 ###############################################################################################################################################
@@ -55,30 +59,91 @@ law run Categorization --version prod_240128 --category-name base --config-name 
 
 # removed zh_hbb_zqq (not availbale ?)
 law run CategorizationWrapper --version prod_240128 --category-names base --config-name ul_2018_ZbbHtt_v10 \
- --dataset-names zh_zbb_htt_signal,zh_zbb_htt_background,zh_hbb_zll,Zprime_Zh_Zbbhtautau_M600,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm \
+ --dataset-names zh_zbb_htt_signal,zh_zbb_htt_background,zh_hbb_zll,Zprime_Zh_Zbbhtautau_M600,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm,zz_sl \
  --PreprocessRDF-version prod_240128 \
  --Categorization-base-category-name base_selection --Categorization-feature-modules-file modulesrdf_Ellipse --workers 100 \
  --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
- --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=long,WNTag=el7'
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=short,WNTag=el7'
 # --skip-dataset-tags secondary \
 
 # ul_2018_ZttHbb_v10
 # removed zh_hbb_zqq (not availbale ?)
 law run CategorizationWrapper --version prod_240128 --category-names base --config-name ul_2018_ZttHbb_v10 \
- --dataset-names zh_ztt_hbb_signal,zh_ztt_hbb_background,zh_htt,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm \
+ --dataset-names zh_ztt_hbb_signal,zh_ztt_hbb_background,zh_htt,zz_bbtt,wjets,dy,ewk_z,ewk_wplus,ewk_wminus,tt_dl,tt_sl,tt_fh,st_tw_antitop,st_tw_top,st_antitop,st_top,zz_dl,zz_fh,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_lnununu,wz_llqq,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,wminush_htt,wplush_htt,tth_bb,tth_tautau,tth_nonb,ggH_ZZ,ggf_sm,zz_sl \
  --PreprocessRDF-version prod_240128 \
  --Categorization-base-category-name base_selection --Categorization-feature-modules-file modulesrdf_Ellipse --workers 100 \
  --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
- --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=long,WNTag=el7'
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=short,WNTag=el7'
 # --skip-dataset-tags secondary \
 
 
 ###############################################################################################################################################
+############################################################  PRECOUNTER  #####################################################################
+###############################################################################################################################################
+# ZH specific 
+law run MergeCategorizationStatsWrapper --version prod_240128 --config-name ul_2018_ZbbHtt_v10 \
+ --dataset-names zh_zbb_htt_signal,zh_zbb_htt_background,zh_hbb_zll,zz_sl,Zprime_Zh_Zbbhtautau_M600,zh_zbb_htt_signal_aux,zh_zbb_htt_background_aux,zh_hbb_zll_aux,zz_sl_aux \
+ --workers 10 --PreCounter-weights-file weights --systematic-names pu
+
+law run MergeCategorizationStatsWrapper --version prod_240128 --config-name ul_2018_ZttHbb_v10 \
+ --dataset-names zh_ztt_hbb_signal,zh_ztt_hbb_background,zh_htt,zh_ztt_hbb_signal_aux,zh_ztt_hbb_background_aux,zh_htt_aux \
+ --workers 5 --PreCounter-weights-file weights --systematic-names pu
+
+# symlinks to ZZ for common datasets (run the loop twice, once in ul_2018_ZbbHtt_v10 and once in ul_2018_ZttHbb_v10)
+cd /grid_mnt/data__data.polcms/cms/cuisset/cmt/MergeCategorizationStats/ul_2018_ZbbHtt_v10
+cd /grid_mnt/data__data.polcms/cms/cuisset/cmt/MergeCategorizationStats/ul_2018_ZttHbb_v10
+for process_dir in /grid_mnt/data__data.polcms/cms/vernazza/cmt/MergeCategorizationStats/ul_2018_ZZ_v10/*; do
+    if [[ ! $(basename $process_dir) == ggXZZbbtt* ]]; then
+        if [[ ! $(basename $process_dir) == zh_hbb_zll* ]]; then # zh_hbb_zll in zh is actually split in sevaral parts, so using it is a mistake
+        if [[ ! $(basename $process_dir) == zz_sl_* ]]; then # signal for ZZ analysis
+            ln -s $process_dir $(basename $process_dir) # also don't forget the _aux datasets
+        fi
+        fi
+    fi
+done 
+# link zz_sl as it is the same for both ZH analyses
+ln -s /grid_mnt/data__data.polcms/cms/cuisset/cmt/MergeCategorizationStats/ul_2018_ZbbHtt_v10/zz_sl /grid_mnt/data__data.polcms/cms/cuisset/cmt/MergeCategorizationStats/ul_2018_ZttHbb_v10/zz_sl
+ln -s /grid_mnt/data__data.polcms/cms/cuisset/cmt/MergeCategorizationStats/ul_2018_ZbbHtt_v10/zz_sl_aux /grid_mnt/data__data.polcms/cms/cuisset/cmt/MergeCategorizationStats/ul_2018_ZttHbb_v10/zz_sl_aux
+
+###############################################################################################################################################
 ############################################################  FeaturePlot  #################################################################
 ###############################################################################################################################################
+###### 1D plots
+# ZbbHtt
+law run FeaturePlot --version prod_240128  --category-name base --config-name ul_2018_ZbbHtt_v10 \
+ --Categorization-base-category-name base_selection --Categorization-feature-modules-file modulesrdf_Ellipse  \
+ --PrePlot2D-workflow local --PrePlot2D-version prod_240128 --PrePlot2D-skip-merging \
+ --MergeCategorizationStats-version prod_240128 --Categorization-version prod_240128 \
+ --feature-names ZHKinFit_mass,ZHKinFit_highmass --skip-dataset-tags secondary --skip-dataset-names 'data_*' \
+ --process-group-name datacard_ZbbHtt --save-root --save-png --stack --workers 20 --print-status 1
 
-law run FeaturePlot2D --version prod_231222 --PrePlot2D-version prod_231222 --PrePlot2D-skip-merging --category-name base --config-name ul_2018_ZH_v10 \
-    --feature-names Ztt_svfit_mass_ellipse:Zbb_mass_ellipse --workers 30 --PrePlot2D-workflow local --MergeCategorizationStats-version prod_231005 --Categorization-version prod_231005 \
-    --dataset-names zh_ztt_hbb_sl_signal,zh_zbb_htt_sl_signal,zh_hbb_zll_background,zh_htt_background,zz_bbtt,dy,tt_dl,tt_sl,tt_fh,wjets,ttw_lnu,ttw_qq,ttww,ttwz,ttwh,ttzh,ttz_llnunu,ttz_qq,ttzz,tth_bb,tth_nonbb,tth_tautau,wplush_htt,wminush_htt,ggH_ZZ,ggf_sm,zz_dl,zz_sl_background,zz_lnu,zz_qnu,wz_lllnu,wz_lnuqq,wz_llqq,wz_lnununu,ww_llnunu,ww_lnuqq,ww_qqqq,zzz,wzz,www,wwz,ewk_z,ewk_wplus,ewk_wminus,st_tw_antitop,st_tw_top,st_antitop,st_top \
-    --process-group-name zh_split_signal --save-root --save-png --stack
+# ZttHbb
+law run FeaturePlot --version prod_240128  --category-name base --config-name ul_2018_ZttHbb_v10 \
+ --Categorization-base-category-name base_selection --Categorization-feature-modules-file modulesrdf_Ellipse  \
+ --PrePlot2D-workflow local --PrePlot2D-version prod_240128 --PrePlot2D-skip-merging \
+ --feature-names ZHKinFit_mass,ZHKinFit_highmass --skip-dataset-tags secondary --skip-dataset-names 'data_*' \
+ --process-group-name datacard_ZttHbb --save-root --save-png --stack --workers 20 --print-status 1
+
+###### Zmass vs Hmass
+# ZbbHtt
+law run FeaturePlot2D --version prod_240128  --category-name base --config-name ul_2018_ZbbHtt_v10 \
+ --PreprocessRDF-version prod_240128 \
+ --Categorization-base-category-name base_selection --Categorization-feature-modules-file modulesrdf_Ellipse  \
+ --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=short,WNTag=el7' \
+ --PrePlot2D-workflow local --PrePlot2D-version prod_240128 --PrePlot2D-skip-merging \
+ --MergeCategorizationStats-version prod_240128 --Categorization-version prod_240128 \
+ --feature-names Htt_svfit_mass_ellipse:Zbb_mass_ellipse --skip-dataset-tags secondary --skip-dataset-names 'data_*' \
+ --process-group-name zh_sig_vs_bkg --save-root --save-png --stack --workers 20 --print-status 1
+
+# ZttHbb
+law run FeaturePlot2D --version prod_240128  --category-name base --config-name ul_2018_ZttHbb_v10 \
+ --PreprocessRDF-version prod_240128 \
+ --Categorization-base-category-name base_selection --Categorization-feature-modules-file modulesrdf_Ellipse  \
+ --Categorization-workflow htcondor --Categorization-htcondor-scheduler llrt3condor.in2p3.fr --Categorization-transfer-logs \
+ --Categorization-custom-condor-tag 'include : /opt/exp_soft/cms/t3/t3queue |,T3queue=short,WNTag=el7' \
+ --PrePlot2D-workflow local --PrePlot2D-version prod_240128 --PrePlot2D-skip-merging \
+ --MergeCategorizationStats-version prod_240128 --Categorization-version prod_240128 \
+ --feature-names Ztt_svfit_mass_ellipse:Hbb_mass_ellipse --skip-dataset-tags secondary --skip-dataset-names 'data_*' \
+ --process-group-name zh_sig_vs_bkg --save-root --save-png --stack --workers 20 --print-status 2
 
