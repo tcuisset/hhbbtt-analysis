115fdc911f409904e76d915ad96d29f90871b8e7
Théo Cuisset <theo.cuisset@polytechnique.edu>
2024-04-04 17:57:49 +0200

    Update ZH commands

--- ToRun/ZH/MovePlotsEOS.sh
+++ ToRun/ZH/MovePlotsEOS.sh
@@ -1,4 +1,5 @@
 
-
-rsync -rltv /grid_mnt/data__data.polcms/cms/cuisset/cmt/FeaturePlot tcuisset@lxplus.cern.ch:/eos/user/t/tcuisset/www/zzbbtt/ --delete \
- --filter '- /*_v10' --filter '- *_bugged/' --filter '- root/'  --filter '- yields/' --filter '+ */'  --filter '+ **_os_iso_*.png' --filter '+ **_os_iso_*.pdf' --filter '- *' 
+# 
+rsync -rltv /grid_mnt/data__data.polcms/cms/cuisset/cmt/FeaturePlot tcuisset@lxplus.cern.ch:/eos/user/t/tcuisset/www/zzbbtt/ --delete --delete-excluded \
+ --filter '- /*_v10' --filter '- **_bugged/' --filter '- **_pilot/' --filter '- **_v1/' --filter '- root/'  --filter '- yields/' --filter '+ */' \
+ --filter '- **/*_datacard_*'  --filter '+ **_os_iso_*.png' --filter '+ **_os_iso_*.pdf' --filter '- *' 
